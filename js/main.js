32a60ebeeaef6a096ca9dee389cbc5dc2b1a9cab
manjon2000 <57468773+manjon2000@users.noreply.github.com>
2022-01-31 11:37:26 +0100

    Delete js directory

--- js/main.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {    
-const itemActived = document.querySelector('.icon-menu-hamburges')
-const openmenu = () => {const itemmenu = document.querySelector('.menu-links') 
-itemmenu.classList.toggle('openmenu')}
-itemActived.addEventListener('click',openmenu);
-});
